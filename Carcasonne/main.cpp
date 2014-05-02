6aa1f5fcf77334d54a46042743083ea4790e04bc
Andreas Schnorr <andreas@schnorr-family.de>
2014-05-02 18:36:25 +0200

    Initial Commit

---
+++ Carcasonne/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Carcasonne
+//
+//  Created by Andreas Schnorr on 02.05.14.
+//  Copyright (c) 2014 Andreas Schnorr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
